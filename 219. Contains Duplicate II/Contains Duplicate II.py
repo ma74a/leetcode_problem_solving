6a2af4b7b34f3ae6206d3a9065dd2708711365af
mahmoud etman <metman835@gmail.com>
2026-06-30 18:23:45 +0300

    adding problem contains duplicate II

---
+++ 219. Contains Duplicate II/Contains Duplicate II.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hs = set()
+        l = 0
+        for r in range(len(nums)):
+            if r - l > k: # check if the window is bigger than k
+                hs.remove(nums[l]) # if, then remove the most left item
+                l += 1
+            if nums[r] in hs:
+                return True
+            hs.add(nums[r])
+        return False
+
+    
+
+# nums = [1,2,3,1]
+nums = [1,2,3,1,2,3]
+k = 2
+# k = 3
+sol = Solution()
+print(sol.containsNearbyDuplicate(nums, k))
