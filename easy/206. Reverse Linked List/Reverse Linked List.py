6d3afd08c3c1807a3c0d10dbcd9655ec3941879d
mahmoud etman <metman835@gmail.com>
2026-08-17 17:50:18 +0300

    adding reverse linkedlist problem

---
+++ easy/206. Reverse Linked List/Reverse Linked List.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Time complexity -> O(n), space compexity -> O(1)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prv = None
+        cur = head
+        nex = head.next
+        while nex is not None:
+            cur.next = prv
+            prv = cur
+            cur = nex
+            nex = nex.next
+
+        return cur
+
+
+
+
