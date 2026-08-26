d0ac3a79ff8af02a383bc8653237f0eaee275473
mahmoud etman <metman835@gmail.com>
2026-08-27 01:02:23 +0300

    adding find closest elements

---
+++ medium/658. Find K Closest Elements/Find K Closest Elements.py
@@ -0,0 +1,42 @@
+from typing import List
+import heapq
+
+# Time complexity -> O(nlog(n)), space compexity -> O(n)
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        distances = []
+        for n in arr:
+            dist = abs(n - x)
+            distances.append((dist, n))
+
+        heapq.heapify(distances)
+        ans = []
+        for i in range(k):
+            dist, num = heapq.heappop(distances)
+            ans.append(num)
+        ans.sort()
+
+        return ans
+
+
+########################################################################################
+
+# Time complexity -> O(nlog(n)), space compexity -> O(n)
+# class Solution:
+#     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+#         distances = []
+#         for n in arr:
+#             dist = abs(n - x)
+#             distances.append((dist, n))
+
+#         distances.sort(key=lambda y: y[0])
+#         ans = []
+#         for i in range(k):
+#             ans.append(distances[i][1])
+
+#         return ans.sort()
+
+
+
+sol = Solution()
+print(sol.findClosestElements(arr = [1,2,3,4,5], k = 4, x = 3))
