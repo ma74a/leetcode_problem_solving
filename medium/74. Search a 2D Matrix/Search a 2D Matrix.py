cdbcc385533eafc6fe066f0c4ba8469f4ac04f0c
mahmoud etman <metman835@gmail.com>
2026-08-31 21:46:06 +0300

    adding search a 2D matrix problem

---
+++ medium/74. Search a 2D Matrix/Search a 2D Matrix.py
@@ -0,0 +1,78 @@
+from typing import List
+
+# Time complexity -> O(log( n * m)), space compexity -> O(n)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_index = self.search_potential_row(matrix, target)
+        if row_index != -1:
+            return self.binary_search_over_row(row_index, matrix, target)
+        
+        return False
+
+
+    def search_potential_row(self, matrix, target):
+        low = 0
+        high = len(matrix) - 1 # no of rows
+        while low <= high:
+            mid = low + (high - low) // 2
+            # print(matrix[mid][len(matrix[0])-1])
+            # print(matrix[mid][0])
+            # print()
+            if matrix[mid][0] <= target and target <= matrix[mid][len(matrix[0])-1]:
+                return mid
+            elif matrix[mid][0] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+
+    def binary_search_over_row(self, row_index, matrix, target):
+        low = 0
+        high = len(matrix[row_index]) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if matrix[row_index][mid] == target:
+                return True
+            elif matrix[row_index][mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return False
+
+
+
+########################################################################################
+
+# Time complexity -> O(m log n), space compexity -> O(n)
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         is_found = False
+#         for row in matrix:
+#             is_found = self.search(row, target)
+#             if is_found:
+#                 return True
+#         return False
+
+#     def search(self, row, target) -> bool:
+#         l, r = 0, len(row) - 1
+
+#         while l <= r:
+#             mid = (l + r) // 2
+#             if row[mid] == target:
+#                 return True
+#             elif row[mid] < target:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+
+#         return False
+
+
+
+
+
+
+sol = Solution()
+print(sol.searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3))
